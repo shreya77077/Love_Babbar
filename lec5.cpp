509240c3c353f5653398220ba04ad4762594d9df
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-09-30 01:05:50 +0530

    init

---
+++ lec5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout<<"enter the value of n"<< endl;
+    cin>> n;
+
+    cout<<"printing count from 1 to n"<< endl;
+    int i = 1;
+    for (; ;){
+       if(i <= n ){
+        cout<< i << endl;
+       }
+       else{
+        break;
+       }
+       i ++;
+    }
+}
