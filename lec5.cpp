00a4e9d0c72bc7612b6321baa9c5a2593cba6130
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-10-01 00:12:30 +0530

    init

--- lec5.cpp
+++ lec5.cpp
@@ -2,19 +2,24 @@
 using namespace std;
 
 int main (){
-    int n;
+    int n,a,b,c;
     cout<<"enter the value of n"<< endl;
-    cin>> n;
+    //cin>> n;
 
     cout<<"printing count from 1 to n"<< endl;
-    int i = 1;
-    for (; ;){
-       if(i <= n ){
-        cout<< i << endl;
-       }
-       else{
-        break;
-       }
-       i ++;
+    // int i = 1;
+    // for (; ;){
+    //    if(i <= n ){
+    //     cout<< i << endl;
+    //    }
+    //    else{
+    //     break;
+    //    }
+    //    i ++;
+    // }
+    for(a = 0,b = 1,c= 2 ; a >=0 && b >= 1 && c >= 2; a--, b -- ,c--){
+        cout << a<<" "<<b<< " "<<c<<endl;
+
     }
+
 }
