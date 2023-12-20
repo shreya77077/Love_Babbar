d5383b7010d6bcf5f1d8a91eaafcfed52204daee
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-12-20 10:39:06 +0530

    init

---
+++ factorial.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+//nCr fact = n!/(n-r)! r!
+int fact(int n){
+   int fact = 1;
+
+   for(int i = 1; i<= n ; i++){
+      fact = fact * i;
+   }
+
+   return fact;
+}
+
+int nCr(int n, int r){
+
+    int num = fact(n);
+
+    int denom = fact(n-r) * fact(r);
+
+    int ans = num/denom ;
+
+    return ans;
+
+
+}
+
+int main(){
+    
+    int n, r;
+
+    cin >> n>>r ;
+   
+    cout <<"Answer is "<< nCr(n,r)<<endl;
+
+     return 0; 
+
+}
