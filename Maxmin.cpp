8fbea15847e44ebcc39b4bcf2f44985b22ae7bb4
Shreya Gupta <shreya.gupta13606@gmail.com>
2023-12-22 01:06:42 +0530

    init

--- Maxmin.cpp
+++ Maxmin.cpp
@@ -1,14 +1,32 @@
 #include<iostream>
 using namespace std;
 
+int getMin(int num[], int n){
+    int min = INT_MIN;
+
+    for(int i = 0; i<n;i++){
+        //int max = INT_MIN;
+        if (num[i] < min){
+           min = num[i];
+
+        }
+        
+    }
+    return min;
+}
+
 int getMax(int num[], int n){
     int max = INT_MIN;
 
     for(int i = 0; i<n;i++){
-        int max = INT_MIN;
-        max = num[i];
+        //int max = INT_MIN;
+        if (num[i] > max){
+           max = num[i];
+
+        }
         
     }
+    return max;
 }
 
 int main(){
@@ -20,6 +38,10 @@ int main(){
     for(int i=0;i<size;i++){
       cin>>num[i];
     }
+
+    cout<< "Maximum value is " << getMax(num , size)<< endl;
+    cout<< "Minimum value is " << getMin(num , size)<< endl;
+
 
     return 0;
 }
